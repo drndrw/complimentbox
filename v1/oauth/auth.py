59942498e2ccdd3b6f309fd24737c96541467fb0
Andrew DaRe <andrew@duh.re>
2018-01-08 23:18:30 -0800

    Added basic auth checks for URL parameters

--- v1/oauth/auth.py
+++ v1/oauth/auth.py
@@ -1,5 +1,6 @@
 # from flask_restful import Resource, Api, reqparse
-from v1 import app
+import os
+from v1 import app, config
 from flask import request
 
 
@@ -27,11 +28,20 @@ https://developers.google.com/actions/identity/oauth2-implicit-flow
 @app.route('/v1/oauth')
 def implicit_auth():
     client_keys = {'google':'Google'} #Validate client ID (eg. Google, Amazon, etc.)
-    client_id = request.args.get('client_id')
-    redirect_uri = request.args.get('redirect_uri')
-    state = request.args.get('state')
-    response_type = request.args.get('response_type')
+
+    #URL arguments to parse
+    client_id = request.args.get('client_id') #Client ID (eg. google)
+    redirect_uri = request.args.get('redirect_uri') #Redirect URI (client side)
+    state = request.args.get('state') #Bookkeeping value; returned unchanged w/ redirect URI
+    response_type = request.args.get('response_type') #Response type should be 'token'
+
+    #Verify client ID
     if client_id in client_keys:
-        return "Match"
-    print(client_id)
-    return "Nada"
+        print('PASSED CLIENT ID: {}'.format(client_id))
+        if redirect_uri == '{}{}'.format(config.dev_config.GOOGLE_PROJECT_ID,config.dev_config.GOOGLE_RECIRECT_URI):
+            print('PASSED REDIRECT URL: {}'.format(redirect_uri))
+            if response_type == 'token':
+                print('PASSED RESPONSE TYPE: {}'.format(response_type))
+                return "Passed all checks. Enable user login."
+    print(config.dev_config.GOOGLE_PROJECT_ID)
+    return "Checks failed. Do not permit user to login"
