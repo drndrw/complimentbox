9ac99c5cbeb952fb668d80eb06ea4524462c258c
Andrew DaRe <andrew@duh.re>
2018-01-09 23:09:20 -0800

    Rewrote conditions with new decorator

--- v1/oauth/auth.py
+++ v1/oauth/auth.py
@@ -3,7 +3,6 @@ import os
 from v1 import app, config
 from flask import request
 
-
 '''
 TEMPORARY UNTIL REACT FRONTEND IS DEVELOPED
 
@@ -16,45 +15,52 @@ More information at:
 https://developers.google.com/actions/identity/oauth2-implicit-flow
 '''
 
-# class Oauth(Resource):
-#
-#     def get(self):
-#         parser = reqparse.RequestParser()
-#         parser.add_argument('yeah')
-#         args = parser.parse_args()
-#         print(args)
-#         return {'test':'test route','args':args}
+#Decorator will add arg to missing_args if conditions aren't met
+class append_missing():
+    def __init__(self):
+        self.missing_args = []
+        self.available_args = {}
+
+    def confirm(self):
+        def wrapper(f):
+            if f(request.args.get(f.__name__)):
+                self.available_args[f.__name__] = request.args.get(f.__name__)
+            else:
+                self.missing_args.append(f.__name__)
+        return wrapper
 
 @app.route('/v1/oauth')
 def implicit_auth():
     client_keys = {'google':'Google'} #Validate client ID (eg. Google, Amazon, etc.)
     missing_args = [] #A list of missing URL arguments (returned to template)
 
-    #URL arguments to parse
+    missing = append_missing()
+
     #Client ID (eg. google)
-    client_id = request.args.get('client_id') if request.args.get('client_id') in client_keys else missing_args.append('client_id')
+    @missing.confirm()
+    def client_id(req):
+        if req in client_keys:
+            return req
+
     #Redirect URI (client side)
-    redirect_uri = request.args.get('redirect_uri') if request.args.get('redirect_uri') == \
-        '{}{}'.format(config.dev_config.GOOGLE_REDIRECT_URI,config.dev_config.GOOGLE_PROJECT_ID)  \
-        else missing_args.append('redirect_uri')
+    @missing.confirm()
+    def redirect_uri(req):
+        if req == '{}{}'.format(config.dev_config.GOOGLE_REDIRECT_URI,config.dev_config.GOOGLE_PROJECT_ID):
+            return req
+
     #Bookkeeping value; returned unchanged w/ redirect URI
-    state = request.args.get('state') if request.args.get('state') else missing_args.append('state')
+    @missing.confirm()
+    def state(req):
+        if req:
+            return req
+
     #Response type should be 'token'
-    response_type = request.args.get('response_type') if request.args.get('response_type') == 'token' \
-        else missing_args.append('response_type')
+    @missing.confirm()
+    def response_type(req):
+        if req == 'token':
+            return req
 
-    if len(missing_args) > 0:
-        return ' '.join(missing_args)
+    if len(missing.missing_args) > 0:
+        return ' '.join(missing.missing_args)
     else:
-        return 'Passed all checks. Enable user login.'
-    #Verify client ID
-    # if client_id in client_keys:
-    #     print('PASSED CLIENT ID: {}'.format(client_id))
-    #     if redirect_uri == '{}{}'.format(config.dev_config.GOOGLE_REDIRECT_URI,config.dev_config.GOOGLE_PROJECT_ID):
-    #         print('PASSED REDIRECT URL: {}'.format(redirect_uri))
-    #         if response_type == 'token':
-    #             print('PASSED RESPONSE TYPE: {}'.format(response_type))
-    #             return "Passed all checks. Enable user login."
-
-    print(config.dev_config.GOOGLE_PROJECT_ID)
-    return "Checks failed. Do not permit user to login"
+        return str(missing.available_args)
