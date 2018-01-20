d2f4c5215a3c4d3215a7b6ef7937c290a925a504
Andrew DaRe <andrew@duh.re>
2018-01-20 11:09:39 -0800

    Added database commit to authorize_user_token()

--- v1/oauth/classes.py
+++ v1/oauth/classes.py
@@ -1,5 +1,5 @@
 from flask import request
-from v1 import models
+from v1 import db, models
 import secrets
 
 #Decorator will add arg to missing_args if conditions aren't met
@@ -22,6 +22,11 @@ class authorize_user_token():
     def __init__(self, username, password):
         self.user = models.User.validate(username, password)
 
-    def create_token(self):
+    #Generates auth token for user. Takes client_id as only parameter (eg. google)
+    def create_token(self, client_id):
         if self.user['status']:
-            return secrets.token_urlsafe(16)
+            token = secrets.token_urlsafe(16)
+            newtoken = models.Authorization(token,self.user['credentials'],client_id)
+            db.session.add(newtoken)
+            db.session.commit()
+            return token
