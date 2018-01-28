0ad8314e7e9289235f1223fdbebe1912f57e7be4
Andrew DaRe <andrew@duh.re>
2018-01-27 18:45:33 -0800

    Added get_messages() method

--- v1/classes.py
+++ v1/classes.py
@@ -1,12 +1,19 @@
 from v1 import db, models
+from v1.models import Messages, MessagesRecipients, User
 
 ############################
 ##### MESSAGES CLASSES #####
 ############################
 
-class Messages():
+class Messages_query():
 
     def __init__(self, user_id):
-        self.user_id = user_id
+        self.user_id = str(user_id)
 
-    # def get_messages(self):
+    def get_messages(self):
+        messages = Messages.query.join(MessagesRecipients, Messages.id == MessagesRecipients.message_id).add_columns(Messages.id, Messages.sender, MessagesRecipients.user_id, MessagesRecipients.read, Messages.message).filter(MessagesRecipients.user_id==self.user_id).all()
+        return messages
+
+    def get_users(self):
+        user_return = User.query.filter(User.id==self.user_id).all()
+        return user_return
