3e6c606b88c3f45b7d9937f3ed40e12785601cf6
Andrew DaRe <andrew@duh.re>
2018-01-27 18:59:30 -0800

    Added loop to iterate through get_messages() messages

--- v1/classes.py
+++ v1/classes.py
@@ -11,8 +11,11 @@ class Messages_query():
         self.user_id = str(user_id)
 
     def get_messages(self):
-        messages = Messages.query.join(MessagesRecipients, Messages.id == MessagesRecipients.message_id).add_columns(Messages.id, Messages.sender, MessagesRecipients.user_id, MessagesRecipients.read, Messages.message).filter(MessagesRecipients.user_id==self.user_id).all()
-        return messages
+        messages = Messages.query.join(MessagesRecipients, Messages.id == MessagesRecipients.message_id).add_columns(Messages.id, Messages.sender, MessagesRecipients.read, Messages.message).filter(MessagesRecipients.user_id==self.user_id).all()
+        messages_return = []
+        for message in messages:
+            messages_return.append({'message_id': message[1], 'sender': message[2], 'read': message[3], 'message': message[4]})
+        return messages_return
 
     def get_users(self):
         user_return = User.query.filter(User.id==self.user_id).all()
