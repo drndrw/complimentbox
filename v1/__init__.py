bb73e16039a503302546e35d480c9f770ab4db1a
Andrew DaRe <andrew@duh.re>
2018-03-09 18:35:08 -0800

    Added sample actions

--- v1/__init__.py
+++ v1/__init__.py
@@ -43,4 +43,21 @@ def identity(payload):
     user_id = payload['identity']
     return user_id
 
+#OAUTH SETTINGS
+from flask_assistant import Assistant, ask, tell
+assist = Assistant(app, route='/v1/google')
+app.config['ASSIST_ACTIONS_ON_GOOGLE'] = True
+
+@assist.action('Default Welcome Intent')
+def greet_and_start():
+    # speech = "Welcome to Compliment box!"
+    # return ask(speech)
+    resp = ask("Welcome to Compliment Box!")
+    resp.card(text='Welcome to Compliment Box! Here we go.',
+              title='Compliment Box',
+              img_url='http://example.com/image.png'
+              )    # resp.link_out('Github Repo', 'https://github.com/treethought/flask-assistant')
+    return resp
+
+
 jwt = JWT(app, authenticate, identity)
