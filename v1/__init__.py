dd3291cbbab634ed9b26d414b882df76175f1f33
Andrew DaRe <andrew@duh.re>
2018-03-12 23:35:35 -0700

    Removed Google actions config

--- v1/__init__.py
+++ v1/__init__.py
@@ -46,18 +46,8 @@ def identity(payload):
 #OAUTH SETTINGS
 from flask_assistant import Assistant, ask, tell
 assist = Assistant(app, route='/v1/google')
-app.config['ASSIST_ACTIONS_ON_GOOGLE'] = True
-
-@assist.action('Default Welcome Intent')
-def greet_and_start():
-    # speech = "Welcome to Compliment box!"
-    # return ask(speech)
-    resp = ask("Welcome to Compliment Box!")
-    resp.card(text='Welcome to Compliment Box! Here we go.',
-              title='Compliment Box',
-              img_url='http://example.com/image.png'
-              )    # resp.link_out('Github Repo', 'https://github.com/treethought/flask-assistant')
-    return resp
+# app.config['ASSIST_ACTIONS_ON_GOOGLE'] = True
+from v1.actions import google
 
 
 jwt = JWT(app, authenticate, identity)
