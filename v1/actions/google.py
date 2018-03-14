a94c608e95b49cbe530f4c05ce192d72bc126781
Andrew DaRe <andrew@duh.re>
2018-03-13 23:17:56 -0700

    Added 'End conversation' intent

--- v1/actions/google.py
+++ v1/actions/google.py
@@ -1,4 +1,5 @@
 from v1 import api, app, assist, db, models, classes
+from flask_assistant import ask, tell
 
 @assist.action('Default Welcome Intent')
 def greet_and_start():
@@ -9,4 +10,22 @@ def greet_and_start():
               title='Compliment Box',
               img_url='http://example.com/image.png'
               )    # resp.link_out('Github Repo', 'https://github.com/treethought/flask-assistant')
+    return resp
+
+@assist.action('Read messages')
+def greet_and_start():
+    # speech = "Welcome to Compliment box!"
+    # return ask(speech)
+    resp = ask("Here is a message. Would you like to hear another one?")
+    resp.card(text='Welcome to Compliment Box! Here we go.',
+              title='Compliment Box',
+              img_url='http://example.com/image.png'
+              )    # resp.link_out('Github Repo', 'https://github.com/treethought/flask-assistant')
+    return resp
+
+@assist.action('End conversation')
+def end_convo():
+    # speech = "Welcome to Compliment box!"
+    # return ask(speech)
+    resp = tell("Thank you for using Compliment Box. Goodbye!")
     return resp
