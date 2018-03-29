f476431df84fa097b1f22a0cff353e9803b15944
Andrew DaRe <andrew@duh.re>
2018-03-28 21:25:47 -0700

    Updated 'Read messages' action

--- v1/actions/google.py
+++ v1/actions/google.py
@@ -1,11 +1,17 @@
-from v1 import api, app, assist, db, models, classes
+# from v1 import api, app, assist, db, models, classes
 from flask_assistant import ask, tell
+from v1.oauth.classes import oauth
+from v1 import api, app, assist, db, models, classes
+from v1.classes import Messages_query
 
+@oauth.oauth_required
 @assist.action('Default Welcome Intent')
 def greet_and_start():
     # speech = "Welcome to Compliment box!"
+    user = oauth.get_id()
+    msg = Messages_query(user)
     # return ask(speech)
-    resp = ask("Welcome to Compliment Box!")
+    resp = ask("Welcome to Compliment Box, {}!".format(msg.username))
     resp.card(text='Welcome to Compliment Box! Here we go.',
               title='Compliment Box',
               img_url='http://example.com/image.png'
@@ -13,14 +19,25 @@ def greet_and_start():
     return resp
 
 @assist.action('Read messages')
-def greet_and_start():
+def read_msg():
     # speech = "Welcome to Compliment box!"
-    # return ask(speech)
-    resp = ask("Here is a message. Would you like to hear another one?")
-    resp.card(text='Welcome to Compliment Box! Here we go.',
-              title='Compliment Box',
-              img_url='http://example.com/image.png'
-              )    # resp.link_out('Github Repo', 'https://github.com/treethought/flask-assistant')
+    user = oauth.get_id()
+    msg = Messages_query(user)
+    message_ids = msg.get_latest_message_id()
+    try:
+        if len(message_ids) > 1:
+            recieved_message = msg.get_individual_message(message_ids[0].message_id)
+            resp = ask(recieved_message[0]['message'])
+            resp.card(text='Message #{}'.format(message_ids[0].id),
+                      title='Message #{}'.format(message_ids[0].id),
+                      img_url='http://example.com/image.png'
+                      )
+        else:
+            recieved_message = msg.get_individual_message(message_ids[0].message_id)
+            resp = ask(recieved_message[0]['message'] + " You have no more messages. What would you like to do now?")
+    except Exception as e:
+        print(e)
+        resp = ask("You have no messages. What would you like to do now?")
     return resp
 
 @assist.action('End conversation')
